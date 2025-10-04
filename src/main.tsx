86ec84cee0c189b97de986d1193335163348d0cc
Fernando Castillo <superferenabled@gmail.com>
2025-10-04 00:05:19 -0600

    added ScrambleWords.tsx for the scramblewords game, changed everything using usestate with no reducer

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import { HooksApp } from './HooksApp';
+// import { HooksApp } from './HooksApp';
+import {ScrambleWords} from "@/05useReducer/ScrambleWords.tsx";
 
 createRoot(document.getElementById('root')!).render(
     <StrictMode>
-        <HooksApp />
+        <ScrambleWords />
     </StrictMode>,
 )
